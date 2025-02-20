298a24a405a529fc7245646ab54bf1c8bd09db36
Ilias_Paralikas <paralikasilias@gmail.com>
2025-02-20 10:22:52 +0200

    modifications

--- early_exit/__init__.py
+++ early_exit/__init__.py
@@ -1,4 +1,3 @@
 from .eenet import EarlyExitNetwork
 from .train import train_model
 from .test import test_all_exits_accuracy
-from .deployment_code import seperate_networks, SegmentedEarlyExitNetwork,test_networks
